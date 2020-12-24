5a901dddcc037ccf9006921919bba221cec7c761
Ara0n <soumik.ghosh1998@gmail.com>
2020-12-25 02:36:49 +0530

    added necessery comments

--- swizzle/__init__.py
+++ swizzle/__init__.py
@@ -10,7 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 '''
 The routes are imported after the database is created because 
-it may led to circular import in python that thorws error
+it may led to circular import in python which is not good
 '''
 from swizzle import routes
 
